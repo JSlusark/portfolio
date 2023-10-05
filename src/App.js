0a7d8e4ca18258581aa8b3e291da8059b501fb98
Jessica Slusark <slusark.jessica@gmail.com>
2023-10-05 13:16:57 +0200

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import Work from "./Components/Work";
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
+			<Home />
+			<Work />
+			<About />
+			<Contact />
 		</div>
 	);
 }
