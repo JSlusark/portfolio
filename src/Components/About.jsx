710066ce8326f3524171c37c2dfe81b5b38c4c1d
Jessica Slusark <slusark.jessica@gmail.com>
2023-10-12 19:00:53 +0200

    improved About component ui

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,35 +6,36 @@ function About() {
 			id="about"
 			className="flex flex-col items-center justify-center min-h-screen bg-stone-200 text-gray-800 p-20"
 		>
-			<div className="px-52">
+			<div className="max-w-4xl px-10">
 				<h1 className="text-7xl font-bold mb-6 w-full text-center">About Me</h1>
-
-				<p className="text-l text-left mb-4">
-					While growing up, I was consistently drawn to gaming, digital art, and
-					the creation of my own digital experiences. This ranged from
-					personalizing blogs to designing graphics and crafting my own games
-					using tools like RPG Maker and Ren'Py.
-				</p>
-				<p className="text-l text-left mb-4">
-					My passion for tech naturally led me into a career where I
-					collaborated closely with developers, supporting and guiding their
-					endeavors with data insights, advertising solutions, trend analysis,
-					and user experience practices. My strength lies in effectively
-					communicating and understanding the development landscape, which
-					propelled my career as a tech sales executive. However, as I delved
-					deeper into my profession, I realized that my true interest was in the
-					development of the services I sold, understanding how they were
-					constructed and considering how they could be improved. This
-					realization inspired me to shift my focus towards becoming a web
-					developer.
-				</p>
-				<p className="text-l text-left mb-6">
-					After gaining some foundational knowledge from online resources and a
-					part-time introductory course at SheCodes, I fully committed to my new
-					path by enrolling in a comprehensive 6-month Full-Stack web
-					development program at CareerFoundry.
-				</p>
-				<div class="grid content-start">
+				<div className="text-lg text-left space-y-4">
+					<p>
+						While growing up, I was consistently drawn to gaming, digital art,
+						and the creation of my own digital experiences. This ranged from
+						personalizing blogs to designing graphics and crafting my own games
+						using tools like RPG Maker and Ren'Py.
+					</p>
+					<p>
+						My passion for tech naturally led me into a career where I
+						collaborated closely with developers, supporting and guiding their
+						endeavors with data insights, advertising solutions, trend analysis,
+						and user experience practices. My strength lies in effectively
+						communicating and understanding the development landscape, which
+						propelled my career as a tech sales executive. However, as I delved
+						deeper into my profession, I realized that my true interest was in
+						the development of the services I sold, understanding how they were
+						constructed and considering how they could be improved. This
+						realization inspired me to shift my focus towards becoming a web
+						developer.
+					</p>
+					<p>
+						After gaining some foundational knowledge from online resources and
+						a part-time introductory course at SheCodes, I fully committed to my
+						new path by enrolling in a comprehensive 6-month Full-Stack web
+						development program at CareerFoundry.
+					</p>
+				</div>
+				<div className="text-left mt-5 mb-10">
 					<a
 						href="file.pdf"
 						download
@@ -43,56 +44,39 @@ function About() {
 						Download CV
 					</a>
 				</div>
-				<div className="grid grid-cols-1 gap-4 w-full mt-6 md:grid-cols-2">
-					<div>
+
+				<div className="grid grid-cols-1 gap-4 w-full mt-10 md:grid-cols-2">
+					<div className="text-left">
 						<h2 className="text-2xl font-semibold">Skills</h2>
-						<div className="flex flex-wrap p-8 ">
-							{/* First Column */}
-							<div className="w-1/2 px-16 text-left">
-								<p className="font-semibold">Front-End:</p>
-								<ul className="pl-6 mb-2">
-									<li>HTML, CSS, Bootstrap</li>
-									<li>JavaScript, React.js / CRA, React Native</li>
-									<li>Angular, Angular Material, jQuery, React Bootstrap</li>
-								</ul>
+						<div className="p-8">
+							<p className="font-semibold">Front-End:</p>
+							<p className=" mb-2">
+								HTML, CSS, Bootstrap, JavaScript, React.js / CRA, React Native,
+								Angular, Angular Material, jQuery, React Bootstrap
+							</p>
 
-								<p className="font-semibold">Back-End:</p>
-								<ul className=" pl-6 mb-2">
-									<li>Node.js, Express, RESTful API</li>
-								</ul>
-							</div>
+							<p className="font-semibold">Back-End:</p>
+							<p className=" mb-2">Node.js, Express, RESTful API</p>
 
-							{/* Second Column */}
-							<div className=" w-1/2 px-16 text-left md:grid-cols-2">
-								<p className="font-semibold">Testing & Debugging:</p>
-								<ul className=" pl-6 mb-2">
-									<li>Jest, Cucumber, Puppeteer</li>
-								</ul>
-
-								<p className="font-semibold">Tools:</p>
-								<ul className=" pl-6 mb-2">
-									<li>
-										Expo, Google Lighthouse, Recharts, Atatus, Parcel, Postman,
-										Git, Github
-									</li>
-								</ul>
-
-								<p className="font-semibold">Databases:</p>
-								<ul className=" pl-6">
-									<li>MongoDB, PostgreSQL, Mongoose, SQL</li>
-								</ul>
-							</div>
+							<p className="font-semibold">Testing & Debugging:</p>
+							<p className=" mb-2">Jest, Cucumber, Puppeteer</p>
+							<p className="font-semibold">Tools:</p>
+							<p className=" mb-2">
+								Expo, Google Lighthouse, Recharts, Atatus, Parcel, Postman, Git,
+								Github
+							</p>
+							<p className="font-semibold">Databases:</p>
+							<p className=" mb-2">MongoDB, PostgreSQL, Mongoose, SQL</p>
 						</div>
 					</div>
-
-					<div className="p-30 text-left">
-						<h2 className="text-2xl font-semibold mb-4">Outside Work</h2>
-						<ul className=" pl-6 ">
-							<li>Exploring the outdoors</li>
-							<li>Reading sci-fi novels</li>
-							<li>Traveling and experiencing new cultures</li>
-							<li>Attending tech meetups</li>
-							{/* Add as many hobbies/activities as you'd like */}
+					<div className="text-left">
+						<h2 className="text-2xl font-semibold">On my free time</h2>
+						<ul className="p-8">
+							<li className=" mb-2">hobby</li>
+							<li className=" mb-2">hobby</li>
+							<li className=" mb-2">hobby</li>
+							<li className=" mb-2">hobby</li>
+							<li className=" mb-2">hobby</li>
 						</ul>
 					</div>
 				</div>
