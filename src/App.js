97de6dbc85e02349be41711205ce27e0490bac1c
Jessica Slusark <slusark.jessica@gmail.com>
2024-02-12 13:13:07 +0100

    Add react-router-dom dependency and implement routing in App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import NavBar from "./Components/NavBar";
 import Home from "./Views/HomeView";
 import About from "./Views/AboutView";
 import Work from "./Views/WorkView";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App bg-slate-900 text-stone-200 ">
-      <NavBar />
-      <Home />
-      <Work />
-      <About />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/home" Component={Home} />
+          <Route path="/work" Component={Work} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,66 +3,84 @@ import { Dialog } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ navigateToPage }) {
   // State variable for mobile menu open/close status.
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   // Menu items object for the navigation bar.
-  const menuItems = ["Home", "Work", "About", "Contact"];
+  const menuItems = ["Home", "Work", "About"];
 
-  // Function to Render Menu Items
-  const renderMenuItem = (item, isMobile = false) => {
-    //this determines the hyperlink for each menu item and mailto for contact link
-    const hrefValue = () => {
-      if (item === "Contact") {
-        return "mailto:slusark.jessica@gmail.com";
-      } else {
-        return "#" + item.toLowerCase();
-      }
-    };
+  // // Function to Render Menu Items
+  // const renderMenuItem = (item, isMobile = false) => {
+  //   //this determines the hyperlink for each menu item and mailto for contact link
+  //   const hrefValue = () => {
+  //     if (item === "Contact") {
+  //       return "mailto:slusark.jessica@gmail.com";
+  //     } else {
+  //       return "#" + item.toLowerCase();
+  //     }
+  //   };
 
-    // Renders the actual JSX for the menu item and changes based on whether it's mobile or desktop.
-    return (
-      <div className={isMobile ? "space-y-2 py-4" : ""}>
-        <a
-          href={`${hrefValue()}`}
-          onClick={(e) => {
-            // Log the clicked link for debugging.
-            console.log("Link clicked:", hrefValue());
+  //   // Renders the actual JSX for the menu item and changes based on whether it's mobile or desktop.
+  //   return (
+  //     <div className={isMobile ? "space-y-2 py-4" : ""}>
+  //       <a
+  //         href={`${hrefValue()}`}
+  //         onClick={(e) => {
+  //           // Log the clicked link for debugging.
+  //           console.log("Link clicked:", hrefValue());
 
-            // If on mobile, this closes the menu after link click.
-            if (isMobile) {
-              setMobileMenuOpen(false);
-            }
-          }}
-          // Conditional styling based on mobile/desktop.
-          className={
-            isMobile
-              ? "mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              : "text-lg font-semibold leading-6 text-slate-300 hover:text-custom-red"
-          }
-        >
-          {item}
-        </a>
-      </div>
-    );
-  };
+  //           // If on mobile, this closes the menu after link click.
+  //           if (isMobile) {
+  //             setMobileMenuOpen(false);
+  //           }
+  //         }}
+  //         // Conditional styling based on mobile/desktop.
+  //         className={
+  //           isMobile
+  //             ? "mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+  //             : "text-lg font-semibold leading-6 text-slate-300 hover:text-custom-red"
+  //         }
+  //       >
+  //         {item}
+  //       </a>
+  //     </div>
+  //   );
+  // };
 
   // Main return for the NavBar component
   return (
-    <header className="bg-transparent md:bg-black  fixed w-full top-0 z-5">
-      <nav className="mx-auto 					flex justify-between p-5 " aria-label="Global">
+    <header className="bg-transparent md:bg-indigo-950  fixed w-full top-0 z-5">
+      <nav className="mx-auto flex justify-between p-5 " aria-label="Global">
         {/* large screens menu */}
         <div className="hidden md:flex gap-x-12 ml-auto">
-          {menuItems.map((item) => renderMenuItem(item))}
+          {menuItems.map((pageName) => {
+            return (
+              <Link
+                to={pageName.toLowerCase()}
+                className="text-lg font-semibold  text-indigo-400 hover:text-custom-red"
+              >
+                {pageName}
+              </Link>
+            );
+          })}
+          <a
+            href="mailto:slusark.jessica@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-lg font-semibold  text-indigo-400 hover:text-custom-red"
+          >
+            Contact
+          </a>
         </div>
 
         {/* hamburger menu on breaking point) */}
         <div className=" flex ml-auto md:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 bg-gray-400"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-indigo-950 bg-indigo-600"
             onClick={() => setMobileMenuOpen(true)}
           >
             <FontAwesomeIcon
@@ -88,7 +106,27 @@ export default function NavBar() {
               </div>
 
               <div className="mt-6 flow-root">
-                {menuItems.map((item) => renderMenuItem(item, true))}
+                {menuItems.map((pageName) => {
+                  return (
+                    <Link
+                      to={pageName.toLowerCase()}
+                      onClick={() => {
+                        setMobileMenuOpen(false);
+                      }}
+                      className="block rounded-lg px-3 py-2 font-semibold leading-7 text-gray-900 hover:bg-gray-100"
+                    >
+                      {pageName}
+                    </Link>
+                  );
+                })}
+                <a
+                  href="mailto:slusark.jessica@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="block rounded-lg px-3 py-2 font-semibold leading-7 text-gray-900 hover:bg-gray-100"
+                >
+                  Contact
+                </a>
               </div>
             </Dialog.Panel>
           </Dialog>
