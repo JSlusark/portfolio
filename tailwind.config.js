b6bdd2eedc9f91a9d21a876cff2b9de9c34941c0
Jessica Slusark <slusark.jessica@gmail.com>
2024-02-19 15:57:08 +0100

    add updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
   theme: {
     extend: {
       colors: {
         "custom-red": "#FF6B6B",
         "transparent-white": "#25254f",
+      },
+      backgroundImage: {
+        "gradient-custom": "linear-gradient(to top, #3f3cbb 10%, #1a202c 90%)",
+      },
+      margin: {
+        "5px": "5px",
       },
     },
   },
